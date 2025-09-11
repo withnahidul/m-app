b1e7f10505ae720e91c53029af4417155996bd0b
withnahidul <nahidulislam6800@gmail.com>
2025-09-11 18:03:49 +0600

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -22,11 +22,11 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+         welcome <code>React</code> test
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        hello Checking app
       </p>
     </>
   )
