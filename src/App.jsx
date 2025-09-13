be73b3dab5edd23d1458c121ea680b5a31f795b1
withnahidul <nahidulislam6800@gmail.com>
2025-09-13 10:29:15 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          number count : {count}
         </button>
         <p>
          welcome <code>React</code> test
